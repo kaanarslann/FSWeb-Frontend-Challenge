e872f7dc85f56e627c61fe38e1ef94cb9efbd18f
kaanarslann <kaanarslan_hp@hotmail.com>
2025-05-02 14:49:13 +0300

    added md-css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,38 +7,38 @@ export default function Profile() {
     const {language, darkMode} = useContext(OptionsContext);
 
     return (
-        <main className="px-28 pt-25 dark:bg-[#252128]" data-theme={darkMode ? "dark" : "light"}>
-            <h2 className="font-semibold text-5xl text-gray-800 dark:text-[#AEBCCF]">{data[language].profileSection.title}</h2>
-            <section className="flex justify-between mt-4">
-                <section className="profile-left flex flex-col w-[45%]">
-                    <h3 className="text-indigo-700 text-3xl font-medium mt-8 mb-8 dark:text-[#B7AAFF]">{data[language].profileSection.profile}</h3>
-                    <div>
-                        <div className="mb-4 flex text-black text-lg leading-[200%] dark:text-white">
+        <section className="px-28 pt-25 dark:bg-[#252128]" data-theme={darkMode ? "dark" : "light"}>
+            <h2 className="font-semibold text-5xl text-gray-800 dark:text-[#AEBCCF] md:text-center lg:text-start">{data[language].profileSection.title}</h2>
+            <section className="flex justify-between mt-4 lg:flex-row md:flex-col">
+                <section className="profile-left flex flex-col lg:w-[45%]">
+                    <h3 className="text-indigo-700 text-3xl font-medium mt-8 mb-8 dark:text-[#B7AAFF] md:text-center lg:text-start">{data[language].profileSection.profile}</h3>
+                    <div className="lg:ml-0 md:ml-20">
+                        <div className="mb-4 flex text-black lg:text-lg md:text-sm leading-[200%] dark:text-white">
                             <span className="font-semibold w-1/2">{data[language].profileSection.birthday}</span>
                             <span className="font-normal">14.09.1992</span>
                         </div>
-                        <div className="mb-4 flex text-black text-lg leading-[200%] dark:text-white">
+                        <div className="mb-4 flex text-black lg:text-lg md:text-sm leading-[200%] dark:text-white">
                             <span className="font-semibold w-1/2">{data[language].profileSection.city}</span>
                             <span className="font-normal">İstanbul</span>
                         </div>
-                        <div className="mb-4 flex text-black text-lg leading-[200%] dark:text-white">
+                        <div className="mb-4 flex text-black lg:text-lg md:text-sm leading-[200%] dark:text-white">
                             <span className="font-semibold w-1/2">{data[language].profileSection.education}</span>
                             <span className="font-normal">İstanbul Aydın Ünv.<br/>Makine Müh. Yüksek Lisans, 2020</span>
                         </div>
-                        <div className="mb-4 flex text-black text-lg leading-[200%] dark:text-white">
+                        <div className="mb-4 flex text-black lg:text-lg md:text-sm leading-[200%] dark:text-white">
                             <span className="font-semibold w-1/2">{data[language].profileSection.role}</span>
                             <span className="font-normal">Frontend, UI</span>
                         </div>
                     </div>
                 </section>
-                <section className="profile-righ w-[55%] pl-25">
-                    <h3 className="text-indigo-700 text-3xl font-medium mt-8 mb-8 dark:text-[#B7AAFF]">{data[language].profileSection.aboutMe}</h3>
-                    <div className="w-[34rem]">
-                        <p className="text-gray-500 text-lg font-normal leading-[150%] mb-10 dark:text-white">{data[language].profileSection.aboutMeText1}</p>
-                        <p className="text-gray-500 text-lg font-normal leading-[150%] dark:text-white">{data[language].profileSection.aboutMeText2}</p>
+                <section className="profile-righ lg:w-[55%] lg:pl-25">
+                    <h3 className="text-indigo-700 text-3xl font-medium mt-8 mb-8 dark:text-[#B7AAFF] md:text-center lg:text-start">{data[language].profileSection.aboutMe}</h3>
+                    <div className="w-[34rem] md:text-center lg:text-start">
+                        <p className="text-gray-500 lg:text-lg md:text-base font-normal leading-[150%] mb-10 dark:text-white">{data[language].profileSection.aboutMeText1}</p>
+                        <p className="text-gray-500 lg:text-lg md:text-base font-normal leading-[150%] dark:text-white">{data[language].profileSection.aboutMeText2}</p>
                     </div>
                 </section>
             </section>
-        </main>
+        </section>
     )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,18 +7,18 @@ export default function Skills() {
     const {language, darkMode} = useContext(OptionsContext);
 
     return (
-        <main className="px-28 pt-25 dark:bg-[#252128]" data-theme={darkMode ? "dark" : "light"}>
+        <section className="px-28 pt-25 dark:bg-[#252128]" data-theme={darkMode ? "dark" : "light"}>
             <h2 className="font-semibold text-5xl text-gray-800 dark:text-[#AEBCCF] md:text-center lg:text-start">{data[language].skillsSection.title}</h2>
             <section className="skills mt-4">
-                <div className="skill-item flex justify-between lg:flex-row md:flex-col">
+                <div className="skill-item flex lg:justify-between md:justify-center lg:flex-row md:flex-col md:text-center lg:text-start">
                     {data[language].skillsSection.skills.map((skill, index) => (
-                        <div key={index} className="w-[18.75rem] h-[6.5rem]">
+                        <div key={index} className="lg:w-[18.75rem] lg:h-[6.5rem] md:w-40 lg:m-0 md:m-auto">
                             <h3 className="text-indigo-700 text-3xl mt-8 font-medium leading-7 dark:text-[#B7AAFF]">{skill.name}</h3>
                             <p className="text-gray-500 font-normal mt-8 text-xs leading-5 dark:text-white">{skill.info}</p>
                         </div>
                     ))}
                 </div>
             </section>
-        </main>
+        </section>
     )
 }
